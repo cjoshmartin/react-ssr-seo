8f3f06d0d705fe97dcf6b0aa5dedf4eca04b075c
Josh Martin <joshmartin@reprogrammed.software>
2025-02-10 01:25:06 -0600

    Prerendering and removing routes
    I think its worth mention in talk about how you could leave old routes in the  app.tsx and still load new routes from the routes.ts file

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,9 @@ import type { Config } from '@react-router/dev/config'
 
 export default {
     appDirectory: "src",
-    ssr: false,
+    ssr: true,
+    async prerender() {
+      return ["/"]
+    },
+
   } satisfies Config;
