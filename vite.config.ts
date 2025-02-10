0299c88b71b65c0c558741bf8ff9cdd1f35d906b
Josh Martin <joshmartin@reprogrammed.software>
2025-02-10 03:56:06 -0600

    try deploying to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { reactRouter } from "@react-router/dev/vite";
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRouter()]
+  plugins: [
+    reactRouter(),
+    netlifyPlugin()
+  ]
 })
