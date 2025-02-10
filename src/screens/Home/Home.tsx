0c7d1bca198c29750055cbacae0a3f44b560db46
Josh Martin <joshmartin@reprogrammed.software>
2025-02-10 03:05:43 -0600

    [WIP]: Metatags

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,6 +4,53 @@ import ThingCard from "../../components/ThingCard/ThingCard";
 import ThingContainer from "../../components/ThingContainer/ThingContainer";
 
 import styles from './Home.module.css';
+import { MetaArgs } from 'react-router';
+
+export function meta({params}: MetaArgs){
+    const title = "Home - Living Things"
+
+    return [
+        {
+            title,
+        },
+        {
+            property: 'twitter:title',
+            content: title,
+        },
+        {
+            property: 'twitter:card',
+            content: 'summary_large_image',
+        },
+        {
+            property: 'description',
+            content: "Welcome to the Living Things site! Here, you will find information about all the living things in Josh's house."
+        },
+        {
+            property: 'og:type',
+            content: "website"
+        },
+        { // TODO
+            property: 'og:url',
+            content: ""
+        },
+        {
+            property: 'og:image',
+            content: "https://cjoshmartin.github.io/react-spa-seo"
+        },
+        {
+            property: 'twitter:image',
+            content: "https://cjoshmartin.github.io/react-spa-seo"
+        },
+        {
+            property: 'og:site_name',
+            content: "Living Things"
+        },
+        {
+            property: 'og:locale',
+            content: "en_US"
+        },
+    ]
+}
 
 export async function loader() {
     return {
